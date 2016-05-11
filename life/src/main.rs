b158d529bd53cf8e475987c0741df7577d4f4c45
laboon <laboon@gmail.com>
2016-05-11 13:13:14 -0400

    looping around works

--- life/src/main.rs
+++ life/src/main.rs
@@ -1,5 +1,12 @@
+extern crate rand;
+
 use std::env;
 use std::process;
+use std::io;
+use std::io::Write;
+
+use rand::Rng;
+
 
 // fn count_neighbors(x: i32, y: i32) -> i32 {
 //     1
@@ -10,6 +17,16 @@ fn print_usage_and_exit() {
     println!("All must be positive integers.");
     println!("percent_alive must be between 0 and 100 (inclusive)");
     process::exit(1);
+}
+
+fn generate_seed() -> i32 {
+    let seed : i32 = rand::thread_rng().gen_range(1, 101);
+    println!("Seed is {}", seed);
+    seed
+}
+
+fn create_world() {
+    let seed = generate_seed();
 }
 
 fn read_args(args: Vec<String>) -> (i32, i32, i32) {
@@ -25,7 +42,8 @@ fn read_args(args: Vec<String>) -> (i32, i32, i32) {
     //     Err(e)   => -1,
     //     Ok(n)    => n,
     // };
-    
+
+    // TODO: Why do I need pointers here?  Because of &args???
     
     (*x, *y, *pct)
 }
@@ -39,5 +57,36 @@ fn main() {
 
     println!("Creating a {} x {} array, {}% alive", x, y, pct);
 
-    // let world = [[bool; x]; y];
+    // There has to be a better way to do this
+    // from http://stackoverflow.com/questions/13212212/creating-two-dimensional-arrays-in-rust
+    let x_usize = x as usize;
+    let y_usize = y as usize;
+    let mut grid_raw = vec![0; x_usize * y_usize];
+    let mut grid_base: Vec<_> = grid_raw.as_mut_slice().chunks_mut(x_usize).collect();
+    let mut grid: &mut [&mut [_]] = grid_base.as_mut_slice();
+    
+    // How to have this read in and mutated? =(    
+    let mut choice = String::new();
+
+    let mut cont = true;
+    
+    while cont {
+
+        print!("[N]ext, [Q]uit > ");
+        io::stdout().flush().ok().expect("Could not flush stdout");
+        
+        io::stdin().read_line(&mut choice);
+
+        println!("You typed: {}", choice.trim());
+
+        match choice.trim() {
+            "Q" => cont = false,
+            "N" => cont = true,
+            _ => println!("Please choose a valid option!"),
+        }
+
+        choice.clear();
+
+        
+    }
 }
