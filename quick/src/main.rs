83b5a806f46d53dcf09ff462245b039bf3ffe2a5
laboon <laboon@gmail.com>
2016-10-18 16:07:54 -0400

    add some more prop tests

--- quick/src/main.rs
+++ quick/src/main.rs
@@ -1,6 +1,7 @@
 extern crate quickcheck;
 
 use quickcheck::quickcheck;
+use quickcheck::TestResult;
 
 // Integer absolute value function
 
@@ -82,6 +83,8 @@ fn selection_sort(mut a: &mut [i32]) {
     }
 }
 
+
+
 // Swap function - given an array a and two locations (p1 & p2),
 // will swap the values in a[p1] and a[p2]
 
@@ -90,6 +93,15 @@ fn swap(a: &mut [i32], p1: usize, p2: usize) {
         let tmp: i32 = a[p1];
         a[p1] = a[p2];
         a[p2] = tmp;
+    }
+}
+
+fn safe_divide(n: f32, d: f32) -> Option<f32> {
+    if d == 0.0 {
+        None
+    } else {
+        let val = n / d;
+        Some(val)
     }
 }
 
@@ -105,7 +117,7 @@ fn main() {
     let mut sort_xs: [i32; 10] = [9, 6, -9, 2, 1, 3, 18, 12, 7, 66];
     selection_sort(&mut sort_xs);
     println!("{:?}", sort_xs);
-    
+
 }
 
 
@@ -226,10 +238,56 @@ fn test_sqrt_prop_lte_orig() {
     quickcheck(prop_sqrt_lte_orig as fn(f32) -> bool);
 }
 
-// #[test]
-// fn test_swap_same_elements() {
-//     fn prop_same_elements(a: [i32], p1: usize, p2: usize) -> bool {
-//         true
-//     }
-//     quickcheck(prop_same_elements as fn([i32], usize, usize) -> bool);
-// }
+// safe_divide-ing with a 0 as the denominator should
+// always return None.  Note that we're generating only the
+// x (numerator) and keeping the denominator constant.
+
+#[test]
+fn test_safe_divide_zero_denom() {
+
+    fn prop_sd_div_0_none(x: f32) -> bool {
+        let r = safe_divide(x, 0.0);
+        match r {
+            Some(_) => false,
+            None => true
+        }
+    }
+    quickcheck(prop_sd_div_0_none as fn(f32) -> bool);
+}
+    
+// safe-dividing with any other value as the denominator
+// should return Some value
+
+// This is more complicated!  We need to throw away any
+// randomly generated pair where y is equal to 0.0.
+// First, note that we are generating TWO different
+// arguments to pass in (this necessitates changing the "as"
+// section to match the function signature).
+
+// Secondly, we need to say that some tests are invalid
+// (any where y == 0).  We can return a TestResult instead of
+// a regular old boolean value, but this will complicate things
+// a bit.  
+
+#[test]
+fn test_safe_divide_nonzero_denom() {
+
+    // Note that we are returning a TestResult here
+    fn prop_sd_div_nonzero_some(x: f32, y: f32) -> TestResult {
+        if y == 0.0 {
+            TestResult::discard()
+        } else {            
+            let r = safe_divide(x, y);
+            match r {
+                Some(_) => TestResult::from_bool(true),
+                None => TestResult::from_bool(false)
+            }
+        }
+    }
+    
+    // we are returning a TestResult here as well
+    quickcheck(prop_sd_div_nonzero_some as fn(f32, f32) -> TestResult);
+}
+
+// Shrinking
+
