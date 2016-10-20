c5d802ae0bf39a6a8cf5486d2a5b03ffc7ed7378
laboon <laboon@gmail.com>
2016-10-20 16:08:29 -0400

    add selection sort vector tests

--- quick/src/main.rs
+++ quick/src/main.rs
@@ -51,50 +51,59 @@ fn sqrt(x: f32) -> f32 {
     mid
 }
 
-// Selection sort function - given an array, will
-// sort in ascending order
+// Selection sort function - given a Vector of i32's, will
+// sort in ascending order if asc = true, else descending
 
-fn selection_sort(mut a: &mut [i32]) {
+fn selection_sort_vec(mut v: &mut Vec<i32>, asc: bool) {
     
     // if 0 or 1 elements, can just return
-    if a.len() < 2 {
+    if v.len() < 2 {
         return
     }
 
     let mut min_index: usize;
     let mut min_val: i32;
 
-    for j in 0..a.len() {
+    for j in 0..v.len() {
         // println!("Checking position {}", j);
         min_index = j;
-        min_val = a[j];
-        for k in (j+1)..a.len() {
+        min_val = v[j];
+        for k in (j+1)..v.len() {
             // println!("\t{} - Comparing {} & {}", k, min_val, a[k]);
-            if a[k] < min_val {
-                // println!("\tSetting min_val to {}", a[k]);
-                min_val = a[k];
-                min_index = k;
+            if asc {
+                if v[k] < min_val {
+                    // println!("\tSetting min_val to {}", a[k]);
+                    min_val = v[k];
+                    min_index = k;
+                }
+            } else {
+                if v[k] > min_val {
+                    // println!("\tSetting min_val to {}", a[k]);
+                    min_val = v[k];
+                    min_index = k;
+                }
+                
             }
         }
-        // println!("{} < {} ?", a[min_index], min_val);
+        // println!("{} < {} ?", v[min_index], min_val);
         // println!("Swapping locs {} & {}", j, min_index);
-        swap(&mut a, j, min_index)
+        swap_vec(&mut v, j, min_index)
 
     }
 }
 
+// Swap function - given a vector v and two locations (p1 & p2),
+// will swap the values in v[p1] and v[p2]
 
-
-// Swap function - given an array a and two locations (p1 & p2),
-// will swap the values in a[p1] and a[p2]
-
-fn swap(a: &mut [i32], p1: usize, p2: usize) {
+fn swap_vec(v: &mut Vec<i32>, p1: usize, p2: usize) {
     if p1 != p2 {
-        let tmp: i32 = a[p1];
-        a[p1] = a[p2];
-        a[p2] = tmp;
+        let tmp: i32 = v[p1];
+        v[p1] = v[p2];
+        v[p2] = tmp;
     }
 }
+
+// Divides two f32's, and wraps the return value in an Option
 
 fn safe_divide(n: f32, d: f32) -> Option<f32> {
     if d == 0.0 {
@@ -105,6 +114,10 @@ fn safe_divide(n: f32, d: f32) -> Option<f32> {
     }
 }
 
+//////////////////////////////////////////////////////////////////
+// Boring main function which runs through a few functions
+//////////////////////////////////////////////////////////////////
+
 fn main() {
     let xs: [f32; 10] = [1.0, 2.0, 3.0, 4.0, 5.0,
     6.0, 7.0, 8.0, 9.0, 10.0];
@@ -114,30 +127,41 @@ fn main() {
         println!("sqrt({}) = {}", x, res);
     }
 
-    let mut sort_xs: [i32; 10] = [9, 6, -9, 2, 1, 3, 18, 12, 7, 66];
-    selection_sort(&mut sort_xs);
-    println!("{:?}", sort_xs);
+    let mut sort_vec = vec![13, 8, 9, 12, 90, 87, 63, 22];
+    selection_sort_vec(&mut sort_vec, true);
+    println!("{:?}", sort_vec);
 
+    selection_sort_vec(&mut sort_vec, false);
+    println!("{:?}", sort_vec);
+    
 }
 
 
 
-// A classic unit test, for comparison
+// A classic unit test, for comparison.
+// Just checks that the absolute value of -1 is 1.
+// Note that we are only checking one specific value
+// and one specific output.
 
 #[test]
 fn test_abs() {
-    assert_eq!(1, abs(1));
+    assert_eq!(1, abs(-1));
 }
+
+////////////////////////////////////////////////////////////////
+// abs(i32) tests
+////////////////////////////////////////////////////////////////
+
 
 // A property-based test.  Note that there are no
 // explicit assertions - they are "built-in" to the
 // quickcheck function.  However, they can be
 // annotated as #[test] just like a normal unit test.
 
-// Absolute values should always be positive
+// Absolute values should never be negative
 
 #[test]
-fn test_abs_prop() {
+fn test_abs_never_negative() {
     // Define a property or properties here.
     // For Rust's version of QuickCheck, I have found
     // that it's better to define one property per test.
@@ -161,6 +185,11 @@ fn test_abs_prop() {
     quickcheck(prop_no_neg as fn(i32) -> bool);
 }
 
+////////////////////////////////////////////////////////////////
+// fabs(i32) tests
+////////////////////////////////////////////////////////////////
+
+
 // Absolute values should always be positive
 
 #[test]
@@ -173,7 +202,6 @@ fn test_fabs_no_neg() {
     quickcheck(prop_fabs_no_neg as fn(f32) -> bool);
 }
 
-
 // Absolute values should always return same value
 
 #[test]
@@ -182,6 +210,7 @@ fn test_fabs_always_same() {
     fn prop_fabs_always_same(x: f32) -> bool {
         fabs(x) == fabs(x)
     }
+    
     quickcheck(prop_fabs_always_same as fn(f32) -> bool);
 }
 
@@ -194,6 +223,10 @@ fn test_fabs_idempotent() {
     }
     quickcheck(prop_fabs_idempotent as fn(f32) -> bool);
 }    
+
+////////////////////////////////////////////////////////////////
+// sqrt tests
+////////////////////////////////////////////////////////////////
 
 
 // Square roots should always return a positive value
@@ -238,7 +271,12 @@ fn test_sqrt_prop_lte_orig() {
     quickcheck(prop_sqrt_lte_orig as fn(f32) -> bool);
 }
 
-// safe_divide-ing with a 0 as the denominator should
+////////////////////////////////////////////////////////////////
+// safe_divide tests
+////////////////////////////////////////////////////////////////
+
+
+// safe_divide-ing with 0.0 as the denominator should
 // always return None.  Note that we're generating only the
 // x (numerator) and keeping the denominator constant.
 
@@ -255,19 +293,24 @@ fn test_safe_divide_zero_denom() {
     quickcheck(prop_sd_div_0_none as fn(f32) -> bool);
 }
     
-// safe-dividing with any other value as the denominator
+// safe-dividing with any nonzero value as the denominator
 // should return Some value
 
 // This is more complicated!  We need to throw away any
 // randomly generated pair where y is equal to 0.0.
 // First, note that we are generating TWO different
 // arguments to pass in (this necessitates changing the "as"
-// section to match the function signature).
+// section to match the function signature).  
 
 // Secondly, we need to say that some tests are invalid
 // (any where y == 0).  We can return a TestResult instead of
 // a regular old boolean value, but this will complicate things
-// a bit.  
+// a bit.
+
+// Finally, note that values are generated and then discarded
+// later.  If too many values are generated which are discarded,
+// quickcheck will eventually give up (specifically, if it can't
+// find 100 examples after 10,000 tries, it will stop trying).
 
 #[test]
 fn test_safe_divide_nonzero_denom() {
@@ -289,5 +332,67 @@ fn test_safe_divide_nonzero_denom() {
     quickcheck(prop_sd_div_nonzero_some as fn(f32, f32) -> TestResult);
 }
 
-// Shrinking
+////////////////////////////////////////////////////////////////
+// selection_sort_vec tests
+////////////////////////////////////////////////////////////////
 
+// Check that the last value in an ascending sort is not smaller than
+// any other value in the vector
+
+#[test]
+fn test_selection_sort_asc_last_element() {
+    fn prop_last_element_not_smaller(mut v: Vec<i32>) -> TestResult {
+        if v.len() < 2 {
+            TestResult::discard()
+        } else {
+            selection_sort_vec(&mut v, true);
+            let last = v.len() - 1;
+            let last_elem = v[last];
+            for j in 0..(last - 1) {
+                if v[j] > last_elem {
+                    return TestResult::from_bool(false);
+                }
+            }
+            TestResult::from_bool(true)
+        }
+    }
+    quickcheck(prop_last_element_not_smaller as fn(Vec<i32>) -> TestResult);
+}
+
+// A sorted array should always have the same number of elements
+
+#[test]
+fn test_selection_sort_same_num_elements() {
+    
+    fn prop_same_num_elems(mut v: Vec<i32>) -> bool {
+        let orig_num = v.len();
+        selection_sort_vec(&mut v, true);
+        orig_num == v.len()
+    }
+    
+    quickcheck(prop_same_num_elems as fn(Vec<i32>) -> bool);
+}
+
+// Sorting an array twice should return the same result as doing it
+// once [idempotency]
+
+#[test]
+fn test_selection_sort_idempotent() {
+
+    fn prop_idempotent(mut v: Vec<i32>) -> bool {
+        selection_sort_vec(&mut v, true);
+        let mut once: Vec<i32> = v.clone();
+        selection_sort_vec(&mut once, true);
+        let twice: Vec<i32> = once.clone();;
+        
+        for j in 0..v.len() {
+            if once[j] != twice[j] {
+                return false
+            }
+        }
+        true
+        
+    }
+    
+    quickcheck(prop_idempotent as fn(Vec<i32>) -> bool);
+}
